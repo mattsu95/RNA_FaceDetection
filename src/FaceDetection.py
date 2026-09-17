61875b99c416e7107e822c337f93b98127601db1
Eduardo N. Korte <dudu.korte@gmail.com>
2026-09-17 00:10:15 -0300

    Otimização via age cache e fps counter

--- src/FaceDetection.py
+++ src/FaceDetection.py
@@ -1,8 +1,7 @@
 import os
-
 import cv2
+import time
 from ultralytics import YOLO
-
 from AgeDetection import AgeDetection
 
 class FaceDetection:
@@ -14,6 +13,10 @@ class FaceDetection:
         self.ad: AgeDetection = age_model
 
         self.window_name = "Face Detecion"
+
+        # Cache age
+        self.age_cache = {}
+        self.frame_count = 0
 
 
     def open_camera(self, camera_index=0):
@@ -33,40 +36,48 @@ class FaceDetection:
         return self.process_frame(bgr_frame)
 
     def process_frame(self, bgr_frame):
-        detection_result = self.face_model(bgr_frame, verbose=False)
+        detection_result = self.face_model.track(bgr_frame, persist=True, verbose=False)
         result = detection_result[0]
 
         result_frame = result.plot()
 
         if self.ad and len(result.boxes) > 0:
-            alt_img, larg_img = bgr_frame.shape[:2]
-            margem = 0.30
+            if result.boxes.id is not None:
+                alt_img, larg_img = bgr_frame.shape[:2]
+                margem = 0.30
 
-            for box in result.boxes:
-                #pega as coord.
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                
-                #calcula margme
-                offset_x = int((x2 - x1) * margem)
-                offset_y = int((y2 - y1) * margem)
+                boxes = result.boxes.xyxy.cpu().numpy().astype(int)
+                track_ids = result.boxes.id.cpu().numpy().astype(int)
 
-                #aplica margem
-                novo_x1 = max(0, x1 - offset_x)
-                novo_y1 = max(0, y1 - offset_y)
-                novo_x2 = min(larg_img, x2 + offset_x)
-                novo_y2 = min(alt_img, y2 + offset_y)
+                for box, track_id in zip(boxes, track_ids):
+                    #pega as coord.
+                    x1, y1, x2, y2 = box
 
-                #Recorte do rosto
-                face_img = bgr_frame[novo_y1:novo_y2, novo_x1:novo_x2]
+                    if track_id not in self.age_cache or self.frame_count % 60 == 0:
+                        #calcula margme
+                        offset_x = int((x2 - x1) * margem)
+                        offset_y = int((y2 - y1) * margem)
 
-                #Calcula a idade
-                idade_predita = self.ad.detect_by_img(face_img)
+                        #aplica margem
+                        novo_x1 = max(0, x1 - offset_x)
+                        novo_y1 = max(0, y1 - offset_y)
+                        novo_x2 = min(larg_img, x2 + offset_x)
+                        novo_y2 = min(alt_img, y2 + offset_y)
 
-                if idade_predita:
-                    cv2.putText(result_frame, f"Idade: {idade_predita}", (x1, max(20, y1 - 10)), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2, cv2.LINE_AA)
+                        #Recorte do rosto
+                        face_img = bgr_frame[novo_y1:novo_y2, novo_x1:novo_x2]
 
+                        # Calcula a idade
+                        self.age_cache[track_id] = self.ad.detect_by_img(face_img)
+
+                    idade_predita = self.age_cache.get(track_id, "")
+                    if idade_predita:
+                        cv2.putText(result_frame, f"Idade: {idade_predita}", (x1, max(20, y1 - 30)), 
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2, cv2.LINE_AA)
+
+        self.frame_count += 1
         face_count = len(result.boxes)
+
         return result_frame, face_count
 
     def release_camera(self):
@@ -77,6 +88,7 @@ class FaceDetection:
     # detecção de rosto através da webcam
     def webcam_capture(self, camera_index=0):
         camera_object = self.open_camera(camera_index)
+        prev_time = 0
 
         try:
             while True:
@@ -84,7 +96,15 @@ class FaceDetection:
                 if not success:
                     break
 
+                #FPS counter
+                current_time = time.time()
+                fps = 1 / (current_time - prev_time)
+                prev_time = current_time                    
+
                 result_frame, _ = self.process_frame(bgr_frame)
+
+                #plot a fps
+                cv2.putText(result_frame, f"FPS: {int(fps)}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3, cv2.LINE_AA)
 
                 cv2.imshow(self.window_name, result_frame)
 
